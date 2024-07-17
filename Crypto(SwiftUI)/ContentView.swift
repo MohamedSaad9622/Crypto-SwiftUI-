71cea433b6421231f2238789a579a5c4a8c79817
mohamed saad <m.saad@majarra.com>
2024-07-17 22:58:50 +0300

    Initial Commit

---
+++ Crypto(SwiftUI)/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Crypto(SwiftUI)
+//
+//  Created by Mohamed Saad on 07/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color.theme.background
+                .ignoresSafeArea()
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crypto(SwiftUI)/Crypto_SwiftUI_App.swift
@@ -0,0 +1,19 @@
+//
+//  Crypto_SwiftUI_App.swift
+//  Crypto(SwiftUI)
+//
+//  Created by Mohamed Saad on 07/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                ContentView()
+            }
+        }
+    }
+}
