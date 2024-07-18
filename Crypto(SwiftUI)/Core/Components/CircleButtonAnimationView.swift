c5359b8cb4826a3bb76501e486566960511ece73
mohamed saad <m.saad@majarra.com>
2024-07-18 11:41:44 +0300

    Create HomeHeaderView and coinModel

---
+++ Crypto(SwiftUI)/Core/Components/CircleButtonAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButtonAnimationView.swift
+//  Crypto(SwiftUI)
+//
+//  Created by Mohamed Saad on 17/07/2024.
+//
+
+import SwiftUI
+
+struct CircleButtonAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButtonAnimationView()
+}
---
+++ Crypto(SwiftUI)/Core/Components/CircleButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButtonView.swift
+//  Crypto(SwiftUI)
+//
+//  Created by Mohamed Saad on 17/07/2024.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButtonView()
+}
