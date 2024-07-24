8cd067e6c9e9529dd37339d326f701edec78b739
Mohamed Saad <m.saad@majarra.com>
2024-07-24 17:18:48 +0300

    add statisticsView

---
+++ Crypto(SwiftUI)/Core/Components/StatisticView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticView.swift
+//  Crypto(SwiftUI)
+//
+//  Created by Mohamed Saad on 24/07/2024.
+//
+
+import SwiftUI
+
+struct StatisticView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatisticView()
+}
---
+++ Crypto(SwiftUI)/StatisticModel.swift
@@ -0,0 +1,8 @@
+//
+//  StatisticModel.swift
+//  Crypto(SwiftUI)
+//
+//  Created by Mohamed Saad on 24/07/2024.
+//
+
+import Foundation
