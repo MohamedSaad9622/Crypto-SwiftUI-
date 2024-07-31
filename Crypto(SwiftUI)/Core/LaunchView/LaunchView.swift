23f433414cdaed4031509b1ea193d7d730ff1fa4
Mohamed Saad <m.saad@majarra.com>
2024-07-31 13:05:59 +0300

    Add LaunchView

---
+++ Crypto(SwiftUI)/Core/LaunchView/LaunchView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchView.swift
+//  Crypto(SwiftUI)
+//
+//  Created by Mohamed Saad on 31/07/2024.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchView()
+}
